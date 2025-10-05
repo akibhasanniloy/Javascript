1bf5d019a780ff7e192773873a56b9272ab16273
akibhasanniloy <akibhasanniloy2@gmail.com>
2025-10-06 02:27:52 +0600

    again starting

---
+++ CN-1 variavle and Data type/varData.js
@@ -0,0 +1,59 @@
+console.log("Welcome to js learning....");
+console.log("I love js");
+console.log("I am eager to learn it from the beginning.")
+
+fullname = "akib"
+age = 21
+price = 99.99
+x = null; // know what is there
+y = undefined; //dont know what will be there
+console.log(fullname)
+console.log(age)
+console.log(price)
+console.log(x)
+console.log(y)
+
+isfollow = false;
+console.log(isfollow)
+isfollow = true;
+console.log(isfollow)
+
+
+
+let a = 10;
+let b = 2;
+let sum = a + b;
+console.log(sum);
+
+const student = {   //write student on browser to see these objects
+    fullName: "akib hasan",
+    age: 20,
+    cgpa: 3.2,
+    isPass: true
+};
+console.log(student);
+console.log(student["age"]);
+
+//Practice queations 
+//1
+const product = {
+    name: "Pen",
+    color: "black",
+    rating: 3,
+    price: 10,
+};
+console.log(product);
+
+//2
+const profile = {
+    userName: "akibhas.an",
+    name: "Akib Hasan",
+    followers: 100,
+    following: 4,
+    posts: 10,
+    bio: "Hello im akib",
+    isFollow: true
+};
+console.log(profile)
+console.log(typeof profile)
+console.log(typeof profile["userName"])
