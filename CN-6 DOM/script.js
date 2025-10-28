d3b15fb90662d57c9772b6df28507000646944a7
akibhasanniloy <akibhasanniloy2@gmail.com>
2025-10-29 03:40:36 +0600

    finish chapter 6

--- CN-6 DOM/script.js
+++ CN-6 DOM/script.js
@@ -1,34 +1,52 @@
-// console.log(document.body.childNodes[2])
-// document.body.childNodes[3].innerText = "abcd";
+// // console.log(document.body.childNodes[2])
+// // document.body.childNodes[3].innerText = "abcd";
 
 
-//Dom manupulation
-let button = document.getElementById("myId");
-console.log(button);
-console.dir(button);
+// //Dom manupulation
+// let button = document.getElementById("myId");
+// console.log(button);
+// console.dir(button);
 
-let headings = document.getElementsByClassName("myClass");
-console.log(headings)
-console.dir(headings)
+// let headings = document.getElementsByClassName("myClass");
+// console.log(headings)
+// console.dir(headings)
 
-let parahs = document.getElementsByTagName("p");
-console.log(parahs);
-console.dir(parahs);
+// let parahs = document.getElementsByTagName("p");
+// console.log(parahs);
+// console.dir(parahs);
 
-//Query selectior --> node list
-let firstEl = document.querySelector("p"); //give us the 1st element
-console.dir(firstEl);
+// //Query selectior --> node list
+// let firstEl = document.querySelector("p"); //give us the 1st element
+// console.dir(firstEl);
 
-let allElements = document.querySelectorAll("p");// for all elements
-console.dir(allElements);
+// let allElements = document.querySelectorAll("p");// for all elements
+// console.dir(allElements);
 
-let allElementsClass = document.querySelectorAll(".myClass");
-console.dir(allElements);
+// let allElementsClass = document.querySelectorAll(".myClass");
+// console.dir(allElements);
 
-//properties for get and set something
-//tagName
-let tagSomething = firstEl.tagName;
-console.log(tagSomething);
-console.dir(tagSomething);
+// //properties for get and set something
+// //tagName
+// let tagSomething = firstEl.tagName;
+// console.log(tagSomething);
+// console.dir(tagSomething);
 
-//innertext
+//main dom
+let div = document.querySelector("div");
+console.dir(div);
+
+let heading = document.querySelector("h1");
+
+let headingPractice = document.querySelector("h2");
+headingPractice.innerText = headingPractice.innerText + " I am Akib";
+console.dir(headingPractice);
+
+let divs = document.querySelectorAll(".box");
+let idx = 1;
+for (div of divs) {
+    div.innerText = `new uniquw value ${idx}`;
+    idx++;
+}
+// divs[0].innerText = "new unique value 1";
+// divs[1].innerText = "new unique value 2";
+// divs[2].innerText = "new unique value 3";
