39bb18794b2b0cb5fd57d7be51a76eec660f2bdd
akibhasanniloy <akibhasanniloy2@gmail.com>
2025-12-02 20:36:09 +0100

    finished lecture 11

---
+++ CN11- Classes and object/app.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 4;
+console.log(a);
+console.log(b);
+console.log("a+b= ", a + b);
+console.log("a+b= ", a - b);
+// console.log("a+b= ", a + c);
+// console.log("a+b= ", a - c);
+
+try {
+  console.log("a+b= ", a + c);
+  console.log("a+b= ", a - c);
+} catch (err) {
+  console.log(err);
+}
+
+console.log("a+b= ", a + b);
+console.log("a+b= ", a - b);
+console.log("a+b= ", a + b);
+
